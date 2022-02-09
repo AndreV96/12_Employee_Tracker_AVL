1fb0dd86690ec1e8995945e11fe0ab410e093505
André Vega <ventaslederer@hotmail.com>
2022-02-08 22:47:50 -0600

    Added packages needed for repository. Also made TODOS for pseudo code of the whole project.

---
+++ server.js
@@ -0,0 +1,8 @@
+//TODO Generate the inquirer questions and functions to navigate the questions
+//TODO: Check one more timethe whole mysql module.
+//TODO: Create schema.sql file based on the image from the README
+//TODO: Create seeds file
+//TODO: Create all sql commands for each speficic prompt asked. Use classes for this?
+//TODO: Use the console.table package to print MySQL rows to the console.
+//TODO: Make README, walkthrough video and upload the project.
+//TODO: ****MAKE sure you are using MySQL2 package and console.table package
